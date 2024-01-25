a5a8eff2d0466ffc01b55d3b3fa4e49f8e57fa33
James Fidell <james@openastroproject.org>
2024-01-25 10:48:21 +0000

    Ditch the mess of "stdafx" files and add a single common #include file for the C++ header stuff that genuinely is common between many files

---
+++ include/dss_common.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <QtCore>
+#include <QtWidgets>
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
+#include <algorithm>
+
+#include "zexcbase.h"
+#include "Ztrace.h"
+#include "zexcept.h"
